3eff0d8032ffc8b65dbc41f2eef155a359b1cdfd
Érico Andrei <erico@simplesconsultoria.com.br>
2011-07-07 14:19:24 -0300

    TestCase and layer use dexterity

--- collective/behavior/contactinfo/tests/test_package.py
+++ collective/behavior/contactinfo/tests/test_package.py
@@ -1,35 +1,67 @@
 # -*- coding:utf-8 -*-
+import unittest
+import transaction
+from Testing.ZopeTestCase import app, close, installProduct, installPackage
 from zope.interface import implements, Interface
 from zope.component import getUtility, getMultiAdapter
-
-from collective.behavior.contactinfo.tests.base import TestCase
+from Products.CMFCore.utils import getToolByName
+from plone.app.dexterity.tests.base import DexterityFunctionalTestCase
 
 from Products.PloneTestCase.setup import default_user
-
+from Products.PloneTestCase.layer import PloneSite
+from zope.app.component.hooks import setSite, setHooks
 from DateTime import DateTime
 
-class DummyEvent(object):
-    implements(IObjectEvent)
-    
-    def __init__(self, object):
-        self.object = object
+import collective.behavior.contactinfo
 
-class TestPackage(TestCase):
+# BBB Zope 2.12
+try:
+    from Zope2.App import zcml
+    from OFS import metaconfigure
+    zcml # pyflakes
+    metaconfigure
+except ImportError:
+    from Products.Five import zcml
+    from Products.Five import fiveconfigure as metaconfigure
 
-    def afterSetUp(self):
-        self.loginAsPortalOwner()
-        # Create objects to be used in test
-        self.portal.invokeFactory('Folder', 'foo')
-    
-    def testSomething(self): 
-        self.assertEquals(1, 1)
-    
+class TestCase(DexterityFunctionalTestCase):
+
+    class layer(PloneSite):
+
+        @classmethod
+        def setUp(cls):
+            metaconfigure.debug_mode = True
+            import plone.app.dexterity
+            zcml.load_config('meta.zcml', plone.app.dexterity)
+            zcml.load_config('configure.zcml', plone.app.dexterity)
+            zcml.load_config('configure.zcml', collective.behavior.contactinfo)
+            metaconfigure.debug_mode = False
+
+            # import the default profile
+            root = app()
+            portal = root.plone
+            setHooks()
+            setSite(portal)
+            tool = getToolByName(portal, 'portal_setup')
+            profile = 'profile-plone.app.dexterity:default'
+            tool.runAllImportStepsFromProfile(profile, purge_old=False)
+            setSite(None)
+            # and commit the changes
+            transaction.commit()
+            close(root)
+
+        @classmethod
+        def tearDown(cls):
+            pass
 
 def test_suite():
     from unittest import TestSuite, makeSuite
     from Testing import ZopeTestCase as ztc
-    suite = ztc.FunctionalDocFileSuite(
-                'netcontactinfo.txt', package='collective.behavior.contactinfo',
-                test_class=TestCase)
+    suite = unittest.TestSuite([ztc.FunctionalDocFileSuite(
+                'netcontactinfo.txt', package='collective.behavior.contactinfo.docs',
+                test_class=TestCase),
+            ztc.FunctionalDocFileSuite(
+                'phonecontactinfo.txt', package='collective.behavior.contactinfo.docs',
+                test_class=TestCase),])
 
     return suite
