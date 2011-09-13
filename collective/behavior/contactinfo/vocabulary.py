54e44f47ec8a7d702c1aff8c3bfa2698f85ff320
Érico Andrei <erico@simplesconsultoria.com.br>
2011-09-13 17:41:47 -0300

    PEP8 fixes

--- collective/behavior/contactinfo/vocabulary.py
+++ collective/behavior/contactinfo/vocabulary.py
@@ -11,11 +11,12 @@ class CountriesVocabulary(object):
     """Vocabulary factory for a list of countries
     """
     grok.implements(IVocabularyFactory)
-    
+    #
     def __call__(self, context):
-        countries = [(v['name'],k) for k, v in _countrylist.items()]
+        countries = [(v['name'], k) for k, v in _countrylist.items()]
         countries.sort()
-        items = [SimpleTerm(k,k,v) for v,k in countries]
+        items = [SimpleTerm(k, k, v) for v, k in countries]
         return SimpleVocabulary(items)
+
 
 grok.global_utility(CountriesVocabulary, name=u"contact.countries")
