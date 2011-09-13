12a6afaa8d7d7aaabfb8a01a97cab41acd3a04c9
Érico Andrei <erico@simplesconsultoria.com.br>
2011-09-13 17:41:27 -0300

    Add factories to all behaviors and PEP8 fixes

--- collective/behavior/contactinfo/behavior/address.py
+++ collective/behavior/contactinfo/behavior/address.py
@@ -5,35 +5,54 @@ from plone.directives import form
 from plone.dexterity.interfaces import IDexterityContent
 from plone.autoform.interfaces import IFormFieldProvider
 
+from collective.behavior.contactinfo.utils import context_property
+
 from collective.behavior.contactinfo import MessageFactory as _
 
-class IAddress(form.Schema):
-   """Marker/Form interface for Address behavior
-   """
-   address = schema.TextLine(
-       title=_(u'Address'),
-       required=False,
-       )
-   
-   city = schema.TextLine(
-       title=_(u'City'),
-       required=True,
-       )
-   
-   state = schema.TextLine(
-       title=_(u'State'),
-       required=False,
-       )
-   
-   postcode = schema.TextLine(
-       title=_(u'Post Code / Zip Code'),
-       required=False,
-       )
-   
-   country = schema.Choice(
-       title=_(u'Country'),
-       required=True,
-       vocabulary='contact.countries',
-       )
 
-alsoProvides(IAddress,IFormFieldProvider)
+class IAddress(form.Schema):
+    """Marker/Form interface for Address behavior
+    """
+    address = schema.TextLine(
+        title=_(u'Address'),
+        required=False,
+        )
+    #
+    city = schema.TextLine(
+        title=_(u'City'),
+        required=True,
+        )
+    #
+    state = schema.TextLine(
+        title=_(u'State'),
+        required=False,
+        )
+    #
+    postcode = schema.TextLine(
+        title=_(u'Post Code / Zip Code'),
+        required=False,
+        )
+    #
+    country = schema.Choice(
+        title=_(u'Country'),
+        required=True,
+        vocabulary='contact.countries',
+        )
+
+
+alsoProvides(IAddress, IFormFieldProvider)
+
+
+class Address(object):
+    """ Factory to store data in attributes
+    """
+    adapts(IDexterityContent)
+    #
+    def __init__(self, context):
+        self.context = context
+    #
+    address = context_property('address')
+    city = context_property('city')
+    state = context_property('state')
+    postcode = context_property('postcode')
+    country = context_property('country')
--- collective/behavior/contactinfo/behavior/contactinfo.py
+++ collective/behavior/contactinfo/behavior/contactinfo.py
@@ -5,54 +5,88 @@ from plone.directives import form
 from plone.dexterity.interfaces import IDexterityContent
 from plone.autoform.interfaces import IFormFieldProvider
 
+from collective.behavior.contactinfo.utils import context_property
+
 from collective.behavior.contactinfo import MessageFactory as _
 
+
 class INetContactInfo(form.Schema):
-   """
-      Marker/Form interface for an Internet set of contact information
-   """
-   
-   email = schema.TextLine(
-       title=_(u'E-mail'),
-       description=_(u'Please provide an email address'),
-       required=False,
-       )
+    """
+       Marker/Form interface for an Internet set of contact information
+    """
+    #
+    email = schema.TextLine(
+        title=_(u'E-mail'),
+        description=_(u'Please provide an email address'),
+        required=False,
+        )
+    #
+    twitter = schema.TextLine(
+        title=_(u'Twitter'),
+        description=_(u'Please provide your Twitter name, if you have one.'),
+        required=False,
+        )
+    #
+    irc_nickname = schema.TextLine(
+        title=_(u'IRC Nickname'),
+        description=_(u'Please provide your IRC nickname, if you have one.'),
+        required=False,
+        )
+    #
+    site = schema.URI(
+        title=_(u'Site'),
+        description=_(u'Please provide the address of your site, blog \
+                        or profile'),
+        required=False,
+        )
 
-   twitter = schema.TextLine(
-       title=_(u'Twitter'),
-       description=_(u'Please provide your Twitter name, if you have one.'),
-       required=False,
-       )
 
-   irc_nickname = schema.TextLine(
-       title=_(u'IRC Nickname'),
-       description=_(u'Please provide your IRC nickname, if you have one.'),
-       required=False,
-       )
+alsoProvides(INetContactInfo, IFormFieldProvider)
 
-   site = schema.URI(
-       title=_(u'Site'),
-       description=_(u'Please provide the address of your site, blog or profile'),
-       required=False,
-       )
 
-alsoProvides(INetContactInfo,IFormFieldProvider)
+class NetContactInfo(object):
+    """ Factory to store data in attributes
+    """
+    adapts(IDexterityContent)
+    #
+    def __init__(self, context):
+        self.context = context
+    #
+    email = context_property('email')
+    twitter = context_property('twitter')
+    irc_nickname = context_property('irc_nickname')
+    site = context_property('site')
+
 
 class IPhoneContactInfo(form.Schema):
-   """
-      Marker/Form interface for a set of telephone contact information
-   """
-   
-   phone = schema.TextLine(
-       title=_(u'Phone'),
-       description=_(u'Please provide a telephone number where you can be reached.'),
-       required=False,
-       )
-   
-   mobile = schema.TextLine(
-       title=_(u'Mobile'),
-       description=_(u'Please provide your mobile phone number.'),
-       required=False,
-       )
+    """
+       Marker/Form interface for a set of telephone contact information
+    """
+    #
+    phone = schema.TextLine(
+        title=_(u'Phone'),
+        description=_(u'Please provide a telephone number where you \
+                        can be reached.'),
+        required=False,
+        )
+    #
+    mobile = schema.TextLine(
+        title=_(u'Mobile'),
+        description=_(u'Please provide your mobile phone number.'),
+        required=False,
+        )
 
-alsoProvides(IPhoneContactInfo,IFormFieldProvider)
+
+alsoProvides(IPhoneContactInfo, IFormFieldProvider)
+
+
+class PhoneContactInfo(object):
+    """ Factory to store data in attributes
+    """
+    adapts(IDexterityContent)
+    #
+    def __init__(self, context):
+        self.context = context
+    #
+    phone = context_property('phone')
+    mobile = context_property('mobile')
