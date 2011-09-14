9ce44442adc194ca12f7c3f102781c7d08bab7ba
Érico Andrei <erico@simplesconsultoria.com.br>
2011-09-14 11:26:22 -0300

    Add plone.app.testing structure

---
+++ collective/behavior/contactinfo/testing.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import FunctionalTesting
+
+
+class Fixture(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # Load ZCML
+        import collective.behavior.contactinfo
+        self.loadZCML(package=collective.behavior.contactinfo)
+
+
+FIXTURE = Fixture()
+INTEGRATION_TESTING = IntegrationTesting(
+    bases=(FIXTURE,),
+    name='collective.behavior.contactinfo:Integration',
+    )
+FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(FIXTURE,),
+    name='collective.behavior.contactinfo:Functional',
+    )
